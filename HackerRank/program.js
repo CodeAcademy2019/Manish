0509c924fc49fac01da11348d55ce666cc07e914
Manish S B <manish_s_b@external.mckinsey.com>
2019-01-19 00:26:59 +0530

    Integrated with Input and Output formatting

--- HackerRank/program.js
+++ HackerRank/program.js
@@ -14,23 +14,58 @@ const calcProfit = (index,inputList)=>{
         }
     }
     if(profit<0){
-        return [[],0]
+        return [[0],0]
     }
-    if(profit==0){
-        return [[index],0]
-    }
+    if(profit===0)
+        profitIndex.push(0)
     return [profitIndex,profit];
 }
 
-const resultFormat = (results)=>{
-    resultString = '';
-    results.forEach((eachPile)=>{
-        
-    })
+const sortNumber = (a,b)=>{
+    return b-a;
 }
 
-const calculate = ()=>{
-    const input = '1\n6 12 3 10 7 16 5\n2\n5 7 3 10 9 10\n8 1 2 3 4 10 16 10 4\n0'
+
+const resultFormat = (results)=>{
+    resultString = '';
+    let i = 0;
+    results.forEach((eachPile)=>{
+        const maxProfit = eachPile.reduce((profit,stackResult)=>{
+            return profit+stackResult[1]
+        },0)
+        const stackPlur = eachPile.reduce((stackPlur,stackResult)=>{
+            stackPlur.push(stackResult[0]);
+            return stackPlur
+        },[])
+        let temp = [];
+        for(let i=1;i<stackPlur.length;i++){
+            for (var x in stackPlur[i-1]){
+                for(var y in stackPlur[i]){
+                    temp.push(stackPlur[i-1][x]+stackPlur[i][y])
+                }
+            }
+            stackPlur[i]=temp;
+            temp = [];
+        }
+        let plursCount = new Set(stackPlur[stackPlur.length-1].sort(sortNumber).reverse())
+        resultString = resultString+(i+1)+'\n';
+        resultString = resultString+maxProfit+'\n'
+        const count = plursCount.length>20 ? 20 : plursCount.length
+        plursCount = [...plursCount]
+        let plursCountString = plursCount.slice(1).reduce((accumulator,presentValue)=>{
+            return accumulator+' '+presentValue
+        },'')
+        plursCountString = plursCount[0] +plursCountString
+        resultString = resultString + plursCountString
+        if(i < results.length-1)
+           resultString = resultString+'\n'
+        i++
+    }) 
+    return resultString;
+}
+
+const calculate = (input)=>{
+    // const input = '1\n6 12 3 10 7 16 5\n2\n5 7 3 10 9 10\n8 1 2 3 4 10 16 10 4\n0'
     let results = []
     inputs = input.split("\n")
     let length = parseInt(inputs[0])
@@ -56,7 +91,7 @@ const calculate = ()=>{
     }
     return resultFormat(results);
 }
-console.log(calcProfit(5,['10','10','10','10','10']))
-// calculate()
+
+calculate('3\n3 19 9 2\n5 20 11 10 7 1\n2 18 2\n0')
 
 module.exports = {calculate,calcProfit}
--- HackerRank/program.test.js
+++ HackerRank/program.test.js
@@ -5,15 +5,48 @@ describe('Profit for single stack',()=>{
         expect(pileOfPurls.calcProfit(5,['7','3','10','9','10'])).toEqual([[4,5],11]);
     })
     it('Zero Profit',()=>{
-        expect(pileOfPurls.calcProfit(5,['10','10','10','10','10'])).toEqual([[5],0]);
+        expect(pileOfPurls.calcProfit(5,['10','10','10','10','10'])).toEqual([[1,2,3,4,5,0],0]);
     })
     it('Negative Profit',()=>{
-        expect(pileOfPurls.calcProfit(5,['10','10','10','10','10'])).toEqual([[5],0]);
+        expect(pileOfPurls.calcProfit(5,['14','13','16','13','14'])).toEqual([[0],0]);
     })
 })
 
 describe('Profit for Multiple Stacks',()=>{
     it('Basic Case',()=>{
-        expect(pileOfPurls.calculate('1\n6 12 3 10 7 16 5\n2\n5 7 3 10 9 10\n8 1 2 3 4 10 16 10 4\n0')).toEqual([[[[4],8]],[[[4,5],11],[[4,5,8],30]]]);
+        expect(pileOfPurls.calculate('1\n6 12 3 10 7 16 5\n2\n5 7 3 10 9 10\n8 1 2 3 4 10 16 10 4\n0')).toEqual('1\n8\n4\n2\n41\n8 9 10 12 13');
+    })
+})
+
+describe('Public Cases',()=>{
+    it('Case 1',()=>{
+        expect(pileOfPurls.calculate('4\n10 16 14 12 15 17 18 1 1 1 1\n10 20 20 10 10 8 18 17 1 1 1\n15 1 2 3 4 15 14 13 12 11 10 20 19 18 17 16\n5 1 1 18 17 1\n0')).toEqual('1\n52\n16');
+    })
+    it('Case 2',()=>{
+        expect(pileOfPurls.calculate('3\n3 19 9 2\n5 20 11 10 7 1\n2 18 2\n0')).toEqual('1\n1\n5 7 8 10');
+    })
+    it('Case 3',()=>{
+        expect(pileOfPurls.calculate('2\n6 1 2 3 10 15 5\n6 15 5 15 5 15 5\n0')).toEqual('1\n24\n3 4 5 6 7 8 9 10 12');
+    })
+    it('Case 4',()=>{
+        expect(pileOfPurls.calculate('1\n10 5 13 8 7 11 9 2 10 11 13\n2\n7 8 1 9 15 3 6 10\n4 7 2 14 10\n0')).toEqual('1\n15\n7 8\n2\n29\n8 9');
+    })
+    it('Case 5',()=>{
+        expect(pileOfPurls.calculate('3\n7 3 2 1 3 10 12 2\n6 12 13 14 15 2 3\n3 1 2 3\n0')).toEqual('1\n62\n16');
+    })
+    it('Case 6',()=>{
+        expect(pileOfPurls.calculate('2\n1 13\n1 16\n4\n6 3 4 10 11 7 13\n3 14 13 10\n2 8 5\n1 14\n0')).toEqual('1\n0\n0\n2\n22\n7');
+    })
+    it('Case 7',()=>{
+        expect(pileOfPurls.calculate('2\n6 10 13 14 13 1 9\n6 6 11 7 15 11 8\n0')).toEqual('1\n6\n3 4 9');
+    })
+    it('Case 8',()=>{
+        expect(pileOfPurls.calculate('3\n7 4 6 7 12 15 4 11\n9 5 9 8 12 6 7 14 6 10\n11 10 5 6 7 12 17 10 8 12 10 9\n0')).toEqual('1\n38\n13 15 16');
+    })
+    it('Case 9',()=>{
+        expect(pileOfPurls.calculate('3\n3 10 10 1\n5 10 1 10 1 10\n4 5 15 10 5\n0')).toEqual('1\n32\n8 9 11 12');
+    })
+    it('Case 10',()=>{
+        expect(pileOfPurls.calculate('2\n5 4 5 6 10 4\n5 9 8 7 4 9\n0')).toEqual('1\n34\n10');
     })
 })
